1e3e21b6f74a73782a4f085d4b459c2348df00a2
tonegi <90652377+tonegi@users.noreply.github.com>
2024-09-23 17:03:06 -0300

    Update main.c

--- main.c
+++ main.c
@@ -2,11 +2,11 @@
 
 int main(void) {
     int years, months, days, age;
-  printf("Type how many years you have: ");
+  printf("Enter how many years you have: ");
       scanf("%d",&years);
-  printf("Type how many months you have: ");
+  printf("Enter how many months you have: ");
       scanf("%d",&months);
-  printf("Type how many days you have: ");
+  printf("Enter how many days you have: ");
       scanf("%d",&days);
     age = years * 365 + months * 30 + days;
   printf("Your age in days is: %d", age);
